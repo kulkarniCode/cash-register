2742cef7d65f1947b3619f3b3839f29bf6428841
Kulkarni Code <87711046+kulkarniCode@users.noreply.github.com>
2021-10-21 07:09:14 +0530

    created app logic and basic styles

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
-const errorMessage = document.querySelector("#error-message");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const tableCaption = document.querySelector("#table-caption");
 
-checkButton.addEventListener("click", () => {
-  errorMessage.style.display = "none";
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+  hideMessage();
   if (billAmount.value > 0) {
     if (cashGiven.value >= billAmount.value) {
-      let amountToBeReturned = cashGiven.value - billAmount.value;
+      const amountToBeReturned = cashGiven.value - billAmount.value;
+      tableCaption.innerText = `Amount to be returned: ${amountToBeReturned}`;
       calculateChange(amountToBeReturned);
     } else {
-      showMessage(
-        "The cash provided should be atleast equal to the bill amount."
-      );
+      showMessage("Do you wanna wash plates?");
     }
   } else {
-    showMessage("The bill amount should be greater than 0.");
+    showMessage("Invalid Bill Amount");
   }
 });
 
-function showMessage(message) {
-  errorMessage.style.display = "block";
-  errorMessage.innerText = message;
+function calculateChange(amountToBeReturned) {
+  for (let i = 0; i < availableNotes.length; i++) {
+    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
 }
 
-function calculateChange() {}
+function hideMessage() {
+  message.style.display = "none";
+}
+
+function showMessage(msg) {
+  message.style.display = "block";
+  message.innerText = msg;
+}
